e0775355411ed8ebb1f317b4a5410bb043a49955
Maryam Ghoreishi <ghoreishi,info@yahoo.com>
2022-12-04 18:22:09 +0330

    Implementing ICategoryRepository

---
+++ src/Services/Shop/Shop.Domain/AggregateModel/CategoryAggregate/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Shop.Domain.Common;
+
+
+namespace Shop.Domain.AggregateModel.CategoryAggregate
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Task<IEnumerable<Category>> GetAsync();
+        Task AddAsync(Category model);
+    }
+}
