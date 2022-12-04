91768a1b377be59f52205f70c9964c4d9b8a8d03
Maryam Ghoreishi <ghoreishi,info@yahoo.com>
2022-12-04 19:07:01 +0330

    Implementing CategoryRepository

---
+++ src/Services/Shop/Shop.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Domain.AggregateModel.CategoryAggregate;
+using Shopping.Domain.Common;
+using Shopping.Infrastructure.Data;
+
+namespace Shopping.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ShopContext _context;
+
+        public CategoryRepository(ShopContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async Task AddAsync(Category model)
+        {
+            await _context.Categories.AddAsync(model);
+        }
+
+        public async Task<IEnumerable<Category>> GetAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
