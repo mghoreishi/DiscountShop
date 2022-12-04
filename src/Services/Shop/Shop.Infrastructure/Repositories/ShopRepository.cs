73c477456fc52e8633edd4970c7374c091636432
Maryam Ghoreishi <ghoreishi,info@yahoo.com>
2022-12-04 19:03:16 +0330

    Impelementing context and ShopRepository

---
+++ src/Services/Shop/Shop.Infrastructure/Repositories/ShopRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Domain.AggregateModel.ShopAggregate;
+using Shopping.Domain.Common;
+using Shopping.Infrastructure.Data;
+
+namespace Shopping.Infrastructure.Repositories
+{
+    public class ShopRepository : IShopRepository
+    {
+        private readonly ShopContext _context;
+
+        public ShopRepository(ShopContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+        public async Task AddAsync(Shop model)
+        {
+            await _context.Shops.AddAsync(model);
+        }
+
+        public async Task<Shop> GetByIdAsync(long id)
+        {
+            return await _context.Shops.Where(q => q.Id == id).FirstOrDefaultAsync();
+        }
+
+        public void Update(Shop model)
+        {
+            _context.Entry(model).State = EntityState.Modified;
+        }
+    }
+}
